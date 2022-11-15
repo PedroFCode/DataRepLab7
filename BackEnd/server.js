8b88b263adeabd089e6c17767883ef4dd100e59f
PedroFGMIT <G00399171@gmit.ie>
2022-11-15 12:30:24 +0000

    lab7

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -9,6 +9,27 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//connecting to mongodb database using mongoose
+const mongoose = require('mongoose');
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect('mongodb+srv://admin:admin@lab7.gy88bru.mongodb.net/?retryWrites=true&w=majority');
+ 
+}
+
+;
+
+//Creating a book schema for database
+const bookSchema = new mongoose.Schema({
+  title:  String, // String is shorthand for {type: String}
+  cover: String,
+  author: String
+  
+});
+
+//Object used to interact with database
+const bookModel = mongoose.model('books', bookSchema);
+
 const cors = require('cors');
 app.use(cors());
 app.use(function (req, res, next) {
@@ -25,49 +46,27 @@ app.get('/', (req, res) => {
 
 app.post('/api/books',(req,res)=>{
   console.log(req.body);
+  bookModel.create({
+    title:req.body.title,
+    cover:req.body.cover,
+    author:req.body.author
+  })
   res.send('Data Recieved');
 })
 
 app.get('/api/books', (req, res) => {
-  const books = [
-    {
-      "title": "Learn Git in a Month of Lunches",
-      "isbn": "1617292419",
-      "pageCount": 0,
-      "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/umali.jpg", "status": "MEAP",
-      "authors": ["Rick Umali"],
-      "categories": []
-    },
-    {
-      "title": "MongoDB in Action, Second Edition",
-      "isbn": "1617291609",
-      "pageCount": 0,
-      "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/banker2.jpg",
-      "status": "MEAP",
-      "authors": [
-        "Kyle Banker",
-        "Peter Bakkum",
-        "Tim Hawkins",
-        "Shaun Verch",
-        "Douglas Garrett"
-      ],
-      "categories": []
-    },
-    {
-      "title": "Getting MEAN with Mongo, Express, Angular, and Node",
-      "isbn": "1617292036",
-      "pageCount": 0,
-      "thumbnailUrl": "https://s3.amazonaws.com/AKIAJC5RLADLUMVRPFDQ.book-thumb-images/sholmes.jpg",
-      "status": "MEAP",
-      "authors": ["Simon Holmes"],
-      "categories": []
-    }
-  ]
-
-  res.json({
-    myBooks: books,
-    message: 'Hello from the server'
+  bookModel.find((error, data)=>{
+      res.json(data);
   })
+})
+
+//Searching for an id in bookModel
+app.get('/api/book/:id',(req, res)=>{
+  console.log(req.params.id);
+
+  bookModel.findById(req.params.id, (error, data)=>{
+      res.json(data);
+  });
 })
 
 app.listen(port, () => {
